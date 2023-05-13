60fa10869847d1b1ec4e35a88868cef753c40e9b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-13 21:01:43 +0600

    updated

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
-
+import React from 'react';
+import Nav from '../Pages/Shared/Nav/Nav';
 import { Outlet } from 'react-router-dom';
-
+import Footer from '../Pages/Shared/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div  className='mx-4 lg:mx-6 my-6'>
-          
+            <Nav></Nav>
             <Outlet></Outlet>
-          
+            <Footer></Footer>
         </div>
     );
 };
